a1c00045687c84ead4a618adb39f0cec433eba28
xiduo <xiduo@deliverik.com>
2020-07-24 15:54:45 +0800

    Initial commit

---
+++ src/test/java/com/yxzd/naruto/NarutoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yxzd.naruto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NarutoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
